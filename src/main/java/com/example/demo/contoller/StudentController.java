597c7fd615104a431ee936a329a2e7aad5e02fd4
bala-amypo <bala@amypo.in>
2025-12-17 09:24:32 +0000

    User commit

--- src/main/java/com/example/demo/contoller/StudentController.java
+++ src/main/java/com/example/demo/contoller/StudentController.java
@@ -1,27 +1,49 @@
+package com.example.demo.controller;
 
-package com.example.demo.service.impl;
-import util.jav.List;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.entity.StudentEntity;
-import com.example.demo.repository.StudentRepository;
 import com.example.demo.service.StudentService;
 
-@Service
-public class StudentServiceImpl implements StudentService {
+
+@RestController
+public class StudentController {
 
     @Autowired
-    StudentRepository repo;
+    StudentService ser;
 
-    @Override
-    public StudentEntity addStudents(StudentEntity student) {
-        return repo.save(student);
+    @PostMapping("/addStudents")
+    public StudentEntity addStudents(@RequestBody StudentEntity student){
+        return ser.addStudents(student);
     }
-    @Override
-    public List<StudentEntity> getStudents(){
+    
+    @GetMapping("/getStudents")
+    public List<StudentEntity> getStudents() {
         return ser.getStudents();
     }
-    @Override
     
+    @GetMapping("/getStudent/{id}")
+    public StudentEntity getStudentById(@PathVariable Long id) {
+        return ser.getStudentById(id);
+    }
+
+    @DeleteMapping("/deleteStudent/{id}")
+    public String deleteStudentById(@PathVariable Long id) {
+        StudentEntity student = ser.getStudentById(id);
+        if(student.isPresent()) {
+            ser.deleteStudentById(id);
+            return "Student deleted successfully.";
+        } else {
+            return "Student not found.";
+        }
+    }
+
 }
